5447fa5e0ad25977c9d89620dcd872b61d6daa08
deepakmewada <mewadadeepak367@gmail.com>
2024-08-23 11:33:49 +0530

    create all remove method (removeFirst(),removeLast,removeAtIndex(),removeElement()) in DublyLinkedList

--- DSA_LinkedList/src/DublyLinkedList.java
+++ DSA_LinkedList/src/DublyLinkedList.java
@@ -13,16 +13,117 @@ public class DublyLinkedList<T> {
     }
 
     public void insertFirst(T element){
+        //I am providing head of node when node is created
+        //or assign next value in node, on node creation time.
         Node node=new Node(element,null,head);
         if(head==null){
             head=node;
-            tail=head;
+            tail=node;
+            size++;
             return;
         }
         head.prives=node;
         head=node;
+        size++;
     }
-    public void display(){
+    public void insertLast(T element){
+        if(head==null){
+            insertFirst(element);
+            return;
+        }
+        //I am providing tail of node when node is created
+        //or assign prives value of node is node creation time.
+        Node node = new Node(element,tail,null);
+        tail.next=node;
+        tail=node;
+        size++;
+    }
+
+    public void insertAt(int index,T element){
+        if(index>size){
+            throw new IndexOutOfBoundsException("index "+index+" Linked List size "+size);
+        }
+        if(index==0){
+            insertFirst(element);
+            return;
+        }
+        if(index==size){
+            insertLast(element);
+            return;
+        }
+        Node tempHead=treversUsingIndex(index);
+        Node node =new Node(element,tempHead.prives,tempHead);
+        tempHead.prives.next=node;
+        tempHead.prives=node;
+        size++;
+    }
+    public T removeFirst(){
+        if(head==null){
+            return null;
+        }
+        T element= head.element;
+        if(head==tail){
+            head=head.next;
+            tail= head;
+            return element;
+        }
+        head=head.next;
+        head.prives=null;
+        return element;
+    }
+
+    public T removeLast(){
+        if(head==tail){
+            return removeFirst();
+        }
+        T element=tail.element;
+        tail=tail.prives;
+        tail.next=null;
+        return element;
+    }
+
+    public T removeAtIndex(int index){
+        if(index==0){
+            return removeFirst();
+        }
+        if(index==size-1){
+            return removeLast();
+        }
+        Node tempHead=treversUsingIndex(index);
+        T element = tempHead.element;
+        tempHead.prives.next=tempHead.next;
+        tempHead.next.prives=tempHead.prives;
+        tempHead.prives=null;
+        tempHead.next=null;
+        return element;
+    }
+    public boolean removeElement(T element){
+        Node tempHead=searchElement(element);
+        if(tempHead==null){
+            return false;
+        }
+        tempHead.prives.next=tempHead.next;
+        tempHead.next.prives=tempHead.prives;
+        tempHead.prives=null;
+        tempHead.next=null;
+        return true;
+    }
+    private Node searchElement(T element){
+        Node temphead=head;
+        while ((temphead!=null)&&(temphead.element!=element)){
+            temphead=temphead.next;
+        }
+        return temphead;
+    }
+    private Node treversUsingIndex(int index){
+        Node tempHead=head;
+        for (int i=0;i<index;i++){
+            tempHead=tempHead.next;
+        }
+        return tempHead;
+    }
+
+    public void printElement(){
         Node tempNode=head;
         while (tempNode!=null){
             System.out.print(tempNode.element+" -> ");
@@ -30,8 +131,21 @@ public class DublyLinkedList<T> {
         }
         System.out.println("END");
     }
+
+    public void printRevers(){
+        printReversElement(head);
+        System.out.println("END");
+    }
+    private void printReversElement(Node tempHead){
+        if(tempHead.next==null){
+            System.out.print(tempHead.element+" -> ");
+            return;
+        }
+        printReversElement(tempHead.next);
+        System.out.print(tempHead.element+" -> ");
+    }
     private class Node{
-        private T element;
+        final private T element;
         private Node prives;
         private Node next;
 
--- DSA_LinkedList/src/DublyLinkedListMainFunction.java
+++ DSA_LinkedList/src/DublyLinkedListMainFunction.java
@@ -6,6 +6,31 @@ public class DublyLinkedListMainFunction {
         list.insertFirst(6);
         list.insertFirst(8);
         list.insertFirst(10);
-        list.display();
+        list.printElement();
+
+        list.insertLast(5);
+        list.insertLast(55);
+        list.printElement();
+
+        list.insertAt(5,90);
+        list.insertAt(0,0);
+        list.insertAt(1,90);
+        list.printElement();
+
+        System.out.println("Size of list = "+list.getSize());
+
+        list.removeFirst();
+        list.printElement();
+
+        list.removeLast();
+        list.printElement();
+
+        list.removeAtIndex(4);
+        list.printElement();
+
+        list.removeElement(6);
+        list.printElement();
+
+        list.printRevers();
     }
 }
